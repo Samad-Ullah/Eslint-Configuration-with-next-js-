68b9e87658b28a52b7e2dc710a8642febb167b5a
Samad Ullah <usamad152@gmail.com>
2022-09-11 07:08:42 -0700

    eslint configuration with prettier

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      hy i am JavaScript Developerhy i am JavaScript Developerhy i am JavaScript
+      Developerhy i am JavaScript Developerhy i am JavaScript Developerhy i am
+      JavaScript Developerhy i am JavaScript Developerhy i am JavaScript
+      Developer. hy i am JavaScript Developerhy i am JavaScript Developer hy i
+      am JavaScript Developerhy i am JavaScript Developerhy i am JavaScript
+      Developer hy i am JavaScript Developer hy i am JavaScript Developer hy i
+      am JavaScript Developerhy i am JavaScript Developer hy i am JavaScript
+      Developer
+    </div>
+  );
+};
+
+export default Home;
